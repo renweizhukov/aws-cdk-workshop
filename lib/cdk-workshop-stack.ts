76772ceb1376e92cc35b94efc88ab7baab66d7bd
Wei Ren <wrn@amazon.com>
2023-03-13 14:54:52 -0700

    Define an API Gateway REST API resource backed by our "hello" function
    https://cdkworkshop.com/20-typescript/30-hello-cdk/400-apigw.html
    
    Signed-off-by: Wei Ren <wrn@amazon.com>

--- lib/cdk-workshop-stack.ts
+++ lib/cdk-workshop-stack.ts
@@ -1,5 +1,5 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
-import * as cdk from 'aws-cdk-lib';
+import * as apigw from 'aws-cdk-lib/aws-apigateway';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import { Construct } from 'constructs';
 
@@ -12,6 +12,11 @@ export class CdkWorkshopStack extends Stack {
       runtime: lambda.Runtime.NODEJS_18_X,    // execution environment
       code: lambda.Code.fromAsset('lambda'),  // code loaded from "lambda" directory
       handler: 'hello.handler'                // file is "hello", function is "handler"
+    });
+
+    // defines an API Gateway REST API resource backed by our "hello" function.
+    new apigw.LambdaRestApi(this, 'Endpoint', {
+      handler: hello
     });
   }
 }
