7d378d57561ff4e2626e259d1ecd0964f32842d2
Wei Ren <wrn@amazon.com>
2023-03-15 15:20:34 -0700

    Add endpoint outputs in CdkWorkshopStack
    https://cdkworkshop.com/20-typescript/70-advanced-topics/200-pipelines/5000-test-actions.html
    
    Signed-off-by: Wei Ren <wrn@amazon.com>

--- lib/cdk-workshop-stack.ts
+++ lib/cdk-workshop-stack.ts
@@ -1,4 +1,4 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
+import { CfnOutput, Stack, StackProps } from 'aws-cdk-lib';
 import * as apigw from 'aws-cdk-lib/aws-apigateway';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import { TableViewer } from 'cdk-dynamo-table-viewer';
@@ -6,6 +6,9 @@ import { Construct } from 'constructs';
 import { HitCounter } from './hitcounter';
 
 export class CdkWorkshopStack extends Stack {
+  public readonly hcViewerUrl: CfnOutput;
+  public readonly hcEndpoint: CfnOutput;
+
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
@@ -21,14 +24,22 @@ export class CdkWorkshopStack extends Stack {
     });
 
     // defines an API Gateway REST API resource backed by our "hello" function.
-    new apigw.LambdaRestApi(this, 'Endpoint', {
+    const gateway = new apigw.LambdaRestApi(this, 'Endpoint', {
       handler: helloWithCounter.handler
     });
 
-    new TableViewer(this, 'ViewHitCounter', {
+    const tv = new TableViewer(this, 'ViewHitCounter', {
       title: 'Hello Hits',
       table: helloWithCounter.table,
       sortBy: '-hits'
+    });
+
+    this.hcEndpoint = new CfnOutput(this, 'GatewayUrl', {
+      value: gateway.url
+    });
+
+    this.hcViewerUrl = new CfnOutput(this, 'TableViewerUrl', {
+      value: tv.endpoint
     });
   }
 }
